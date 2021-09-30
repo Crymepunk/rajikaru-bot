1aaf94722373e51072c52a3c2ccf05d36180f886
SauceyRed <csred@protonmail.com>
2021-09-30 15:52:08 +0200

    Merged on_message() events

--- bot.py
+++ bot.py
@@ -15,36 +15,32 @@ bot = commands.Bot(command_prefix=commands.when_mentioned_or('?'))
 
 @bot.event
 async def on_ready():
-    print("Running on {0.user}".format(bot))
+    print(f"Running on {bot.user}")
 
 @bot.command(aliases=['av'])
 async def avatar(ctx, member: nextcord.Member = None):
     """Syntax: avatar (member)"""
     if(member != None):
-        await ctx.send(member.avatar_url)
+        await ctx.send(member.avatar)
     else:
-        await ctx.send(ctx.author.avatar_url)
+        await ctx.send(ctx.author.avatar)
 
 @bot.event
 async def on_message(message):
-    if message.author.bot: 
+    print("here")
+    if message.author.bot:
         return
-
-    if 'balls' in message.content and message.channel.id == 891479236857196624:
+    if 'balls' in message.content and message.channel.id == 893098761448996886:
         await message.channel.send("<@384380563861274625> sucking balls 2021 (colorised) https://media.discordapp.net/attachments/885882663670546432/887437098972508180/unknown.png")
+    if message.content.startswith('lilly ping poggers') and message.author.id == bot.owner_id:
+        while True:
+            await message.channel.send("follow <@621041670309543967>'s patreon https://www.patreon.com/underagegameruwu18pluspoorgibmoneyhelp")
+            sleep(3)
+    await bot.process_commands(message)
 
 @bot.command()
 async def say(ctx, *, arg):
     await ctx.send(arg)
-
-@bot.event
-async def on_message(message):
-    if message.author == 769632057575342081:
-        return
-    if message.content.startswith('lilly ping poggers'):
-        while True:
-            await message.channel.send("follow <@621041670309543967>'s patreon https://www.patreon.com/underagegameruwu18pluspoorgibmoneyhelp")
-            sleep(3)
 
 token = open("token","r").readline()
 bot.run(token)
