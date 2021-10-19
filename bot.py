2ef1b3d9a7b2f0c83566262185bc1241cb6d0bcb
Benjamin J <benjaminjohnsen.bgj@gmail.com>
2021-10-19 11:22:29 +0200

    added kick/ban commands

--- bot.py
+++ bot.py
@@ -4,13 +4,14 @@ import os
 from time import sleep
 from nextcord.ext import commands
 
-logger = logging.getLogger('discord')
+#Makes program log to discord.log file
+logger = logging.getLogger('nextcord')
 logger.setLevel(logging.DEBUG)
 handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
 handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
 logger.addHandler(handler)
 
-#client = discord.Client()
+#Defines bot and adds a prefix
 bot = commands.Bot(command_prefix=commands.when_mentioned_or('?'))
 
 @bot.event
@@ -25,14 +26,27 @@ async def avatar(ctx, member: nextcord.Member = None):
     else:
         await ctx.send(ctx.author.avatar)
 
+@bot.command()
+@commands.has_guild_permissions(ban_members=True)
+async def ban(ctx, member: nextcord.Member, *, reason=None):
+    """Syntax: ban [member]"""
+    await member.ban(reason=reason)
+    await ctx.send(f"{member} has been banned for {reason}")
+
+@bot.command()
+@commands.has_guild_permissions(kick_members=True)
+async def kick(ctx, member: nextcord.Member, *, reason=None):
+    """Syntax: kick [member]"""
+    await member.kick(reason=reason)
+    await ctx.send(f"{member} has been kicked for {reason}")
+
 @bot.event
 async def on_message(message):
-    print("here")
     if message.author.bot:
         return
-    if 'balls' in message.content and message.channel.id == 893098761448996886:
+    if 'balls' in message.content and message.channel.id == 891479236857196624:
         await message.channel.send("<@384380563861274625> sucking balls 2021 (colorised) https://media.discordapp.net/attachments/885882663670546432/887437098972508180/unknown.png")
-    if message.content.startswith('lilly ping poggers') and message.author.id == bot.owner_id:
+    if message.content.startswith('woo ping lui') and message.author.id == bot.owner_id:
         while True:
             await message.channel.send("follow <@621041670309543967>'s patreon https://www.patreon.com/underagegameruwu18pluspoorgibmoneyhelp")
             sleep(3)
