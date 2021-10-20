e0238215ef1d51b72cd125c9ba3f5569c5c06d11
Benjamin J <benjaminjohnsen.bgj@gmail.com>
2021-10-20 02:01:37 +0200

    This doesnt work

--- bot.py
+++ bot.py
@@ -18,6 +18,8 @@ bot = commands.Bot(command_prefix=commands.when_mentioned_or('?'))
 async def on_ready():
     print(f"Running on {bot.user}")
 
+bot.load_extension("cogs.moderation")
+
 @bot.command(aliases=['av'])
 async def avatar(ctx, member: nextcord.Member = None):
     """Syntax: avatar (member)"""
@@ -25,20 +27,6 @@ async def avatar(ctx, member: nextcord.Member = None):
         await ctx.send(member.avatar)
     else:
         await ctx.send(ctx.author.avatar)
-
-@bot.command()
-@commands.has_guild_permissions(ban_members=True)
-async def ban(ctx, member: nextcord.Member, *, reason=None):
-    """Syntax: ban [member]"""
-    await member.ban(reason=reason)
-    await ctx.send(f"{member} has been banned for {reason}")
-
-@bot.command()
-@commands.has_guild_permissions(kick_members=True)
-async def kick(ctx, member: nextcord.Member, *, reason=None):
-    """Syntax: kick [member]"""
-    await member.kick(reason=reason)
-    await ctx.send(f"{member} has been kicked for {reason}")
 
 @bot.event
 async def on_message(message):
---
+++ cogs/moderation.py
@@ -0,0 +1,26 @@
+import nextcord
+from nextcord.ext import commands
+
+class Moderation(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    @commands.has_guild_permissions(ban_members=True)
+    async def ban(ctx, member: nextcord.Member, *, reason="No reason provided."):
+        """Syntax: ban [member]"""
+        if commands.has_guild_permissions(manage_messages=True) or commands.has_guild_permissions(administrator=True):
+            await ctx.send("Can't ban a Moderator")
+        elif commands.has_guild_permissions(manage_messages=False) and commands.has_guild_permissions(administrator=False):
+            await member.ban(reason=reason)
+            await ctx.send(f'{member} has been banned for "{reason}"')
+
+    @commands.command()
+    @commands.has_guild_permissions(kick_members=True)
+    async def kick(ctx, member: nextcord.Member, *, reason=None):
+        """Syntax: kick [member]"""
+        await member.kick(reason=reason)
+        await ctx.send(f'{member} has been kicked for "{reason}"')
+
+def setup(bot):
+    bot.add_cog(Moderation(bot))
