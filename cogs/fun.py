a113ff41656488a3c85f8ba1846a4fcf4925f928
Radical <benjaminjohnsen.bgj@gmail.com>
2021-10-26 12:16:33 +0200

    Made more changes

--- cogs/fun.py
+++ cogs/fun.py
@@ -1,6 +1,7 @@
 import nextcord
 from nextcord.ext import commands
 import nekos
+from time import sleep
 
 class Fun(commands.Cog):
     def __init__(self, bot):
@@ -17,15 +18,25 @@ class Fun(commands.Cog):
             return
         if 'balls' in message.content and message.channel.id == 891479236857196624:
             await message.channel.send("<@384380563861274625> sucking balls 2021 (colorised) https://media.discordapp.net/attachments/885882663670546432/887437098972508180/unknown.png")
-        if message.content.startswith('woo ping lui') and message.author.id == bot.owner_id:
+        if message.content.startswith('woo ping lui') and message.author.id == 769632057575342081:
             while True:
                 await message.channel.send("follow <@621041670309543967>'s patreon https://www.patreon.com/underagegameruwu18pluspoorgibmoneyhelp")
                 sleep(3)
-    
+
     @commands.command()
     async def owoify(self, ctx, *, message):
         """Owoifies your text."""
         await ctx.send(nekos.owoify(text=message))
+
+    @commands.command()
+    async def neko(self, ctx):
+        await ctx.send(nekos.img(target="neko"))
+
+    @commands.command()
+    async def pat(self, ctx, member: nextcord.Member):
+        pat = discord.Embed(title="", description="", color=0x32363c)
+        pat.set_image(url=f"{nekos.img(target='pat')}")
+        await ctx.send_message(message.channel, f"{ctx.author}, embed=pat)
 
 def setup(bot):
     bot.add_cog(Fun(bot))
