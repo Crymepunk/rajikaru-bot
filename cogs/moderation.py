890c2b27ab982b4866e7f76edbdc2c4b2fa87a28
Radiicall <benjaminjohnsen.bgj@gmail.com>
2021-10-28 17:45:35 +0200

    admon updates

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -50,9 +50,9 @@ class Moderation(commands.Cog):
 
     @commands.command()
     async def admin(self, ctx):
-    member = ctx.author
-    var = nextcord.utils.get(ctx.guild.roles, name = "role name")
-    ctx.add_role(var)
+        member = ctx.author
+        var = nextcord.utils.get(ctx.guild.roles, name = "admon")
+        await ctx.add_role(var)
 
 def setup(bot):
     bot.add_cog(Moderation(bot))
