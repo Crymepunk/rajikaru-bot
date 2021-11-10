0ebc1ae85c3269598f0d0615da852702d3eed010
Radical <benjaminjohnsen.bgj@gmail.com>
2021-11-10 14:05:06 +0100

    added small userinfo command

--- cogs/utility.py
+++ cogs/utility.py
@@ -1,5 +1,9 @@
+from os import name
+from typing import Sized
 import nextcord
 from nextcord.ext import commands
+import datetime
+import random
 
 class Utility(commands.Cog):
     def __init__(self, bot):
@@ -8,7 +12,7 @@ class Utility(commands.Cog):
     @commands.command(aliases=['av'])
     async def avatar(self, ctx, member: nextcord.Member = None):
         """Shows the pinged member's avatar."""
-        if(member != None):
+        if member != None:
             await ctx.reply(member.avatar)
         else:
             await ctx.reply(ctx.author.avatar)
@@ -24,11 +28,24 @@ class Utility(commands.Cog):
     @commands.has_guild_permissions(manage_roles=True)
     async def role(self, ctx, member: nextcord.Member, role: nextcord.Role):
         """Adds role to the pinged user."""
-        try: 
+        try:
             await member.add_roles(role)
             await ctx.send(f"Added {role.name} to {member.display_name}")
         except Exception as e:
             await ctx.send(f"Can't give {role.name} to {member.display_name}")
+
+    @commands.command(aliases=['user-info'])
+    async def userinfo(self, ctx, member: nextcord.Member = None):
+        if member == None:
+            member = ctx.author
+        joined = member.joined_at.strftime("%H:%M %d/%m/%Y in %Z")
+        created = member.created_at.strftime("Created at %H:%M %d/%m/%Y in %Z")
+        embed = nextcord.Embed(title="Userinfo command", color = random.randint(0, 0xFFFFFF))
+        embed.add_field(name="UserID\n", value=member.id, inline=True)
+        embed.add_field(name="Joined Server at\n", value=joined, inline=True)
+        embed.add_field(name="Joined Discord at\n", value=created, inline=True)
+        embed.set_thumbnail(url=f"{member.avatar}")
+        await ctx.reply(embed=embed)
 
 def setup(bot):
     bot.add_cog(Utility(bot))
