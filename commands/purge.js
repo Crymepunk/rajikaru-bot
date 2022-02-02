7434bf67d1671d2f80cd79bd423383dd0c05c137
Radical <benjaminjohnsen.bgj@gmail.com>
2022-02-02 20:30:44 +0100

    Fixed purge command after several hours of debugging

--- commands/purge.js
+++ commands/purge.js
@@ -1,5 +1,5 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
-const { Permissions, Guild } = require('discord.js');
+const { Permissions } = require('discord.js');
 
 module.exports = {
 	data: new SlashCommandBuilder()
@@ -8,15 +8,15 @@ module.exports = {
         .addIntegerOption(option => option.setName('amount').setDescription('Amount of messages to delete.').setRequired(true)),
 	async execute(interaction) {
         const limit = interaction.options.getInteger('amount');
-        if (Guild.me.member.permissionsIn(interaction.channel).has(Permissions.FLAGS.MANAGE_MESSAGES)) {
+        if (interaction.guild.me.permissionsIn(interaction.channel).has(Permissions.FLAGS.MANAGE_MESSAGES)) {
             if (interaction.member.permissionsIn(interaction.channel).has(Permissions.FLAGS.MANAGE_MESSAGES)) {
-                await interaction.channel.bulkDelete(limit + 1);
+                await interaction.channel.bulkDelete(limit);
                 await interaction.reply(`Chat purged by ${interaction.user}`);
             } else {
-                interaction.reply({ content: 'You are missing the **Manage Messages** permission.', ephemeral: true });
+                await interaction.reply({ content: 'You are missing the **Manage Messages** permission.', ephemeral: true });
             }
         } else {
-            interaction.reply({ content: 'I am missing the **Manage Messages** permission.', ephemeral: true });
+            await interaction.reply({ content: 'I am missing the **Manage Messages** permission.', ephemeral: true });
         }
 	},
 };
