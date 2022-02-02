76410e0860fef7d8013794e4c9e30bc8b2518ba1
Radical <benjaminjohnsen.bgj@gmail.com>
2022-02-02 11:54:33 +0100

    removed friendserver from the config to make the bot more open

--- commands/purge.js
+++ commands/purge.js
@@ -1,5 +1,4 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
-const { friendserver } = require('../config.json');
 const { Permissions, Guild } = require('discord.js');
 
 module.exports = {
@@ -8,18 +7,16 @@ module.exports = {
 		.setDescription('Removes messages.')
         .addIntegerOption(option => option.setName('amount').setDescription('Amount of messages to delete.').setRequired(true)),
 	async execute(interaction) {
-        if (interaction.guild != friendserver) {
-            const limit = interaction.options.getInteger('amount');
-            if (Guild.me.member.permissionsIn(interaction.channel).has(Permissions.FLAGS.MANAGE_MESSAGES)) {
-                if (interaction.member.permissionsIn(interaction.channel).has(Permissions.FLAGS.MANAGE_MESSAGES)) {
-                    await interaction.channel.bulkDelete(limit + 1);
-                    await interaction.reply(`Chat purged by ${interaction.user}`);
-                } else {
-                    interaction.reply({ content: 'You are missing the **Manage Messages** permission.', ephemeral: true });
-                }
+        const limit = interaction.options.getInteger('amount');
+        if (Guild.me.member.permissionsIn(interaction.channel).has(Permissions.FLAGS.MANAGE_MESSAGES)) {
+            if (interaction.member.permissionsIn(interaction.channel).has(Permissions.FLAGS.MANAGE_MESSAGES)) {
+                await interaction.channel.bulkDelete(limit + 1);
+                await interaction.reply(`Chat purged by ${interaction.user}`);
             } else {
-                interaction.reply({ content: 'I am missing the **Manage Messages** permission.', ephemeral: true });
+                interaction.reply({ content: 'You are missing the **Manage Messages** permission.', ephemeral: true });
             }
+        } else {
+            interaction.reply({ content: 'I am missing the **Manage Messages** permission.', ephemeral: true });
         }
 	},
 };
