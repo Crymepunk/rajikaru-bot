0fa44feecf97fc01533cbbf0d1f80658ee5a3e5a
Radical <benjaminjohnsen.bgj@gmail.com>
2022-02-04 02:16:14 +0100

    Added permission and role checks

--- commands/nick.js
+++ commands/nick.js
@@ -1,4 +1,5 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
+const { Permissions } = require('discord.js');
 
 module.exports = {
 	data: new SlashCommandBuilder()
@@ -7,9 +8,17 @@ module.exports = {
         .addUserOption(option => option.setName('member').setDescription('Select a user').setRequired(true))
         .addStringOption(option => option.setName('nick').setDescription('New Nickname').setRequired(true)),
 	async execute(interaction) {
-        const user = interaction.options.getMember('member');
+        const member = interaction.options.getMember('member');
         const nick = interaction.options.getString('nick');
-        user.edit({ nick: nick });
-        interaction.reply(`Changed ${user.displayName}'s nickname to ${user.user}`);
+        const usrole = interaction.member.roles.highest;
+        const memrole = member.roles.highest;
+
+        if (usrole.comparePositionTo(memrole) <= memrole.comparePositionTo(usrole)) {
+            interaction.reply({ content: 'Cannot change nickname of someone with the same or higher rank as you.', ephemeral: true });
+            return;
+        } else if (interaction.member.permissions.has(Permissions.FLAGS.MANAGE_NICKNAMES)) {
+            member.edit({ nick: nick });
+            interaction.reply(`Changed ${member.displayName}'s nickname to ${member.user}`);
+        }
     },
 };
