cc1f71e5ee15336e589191d1bc3dc3a344c7059c
Radical <benjaminjohnsen.bgj@gmail.com>
2022-02-05 18:23:04 +0100

    Added a permission check and bad word check

--- commands/say.js
+++ commands/say.js
@@ -1,4 +1,5 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
+const { Permissions } = require('discord.js');
 
 module.exports = {
 	data: new SlashCommandBuilder()
@@ -7,6 +8,13 @@ module.exports = {
         .addStringOption(option => option.setName('message').setDescription('Message to say').setRequired(true)),
 	async execute(interaction) {
         const message = interaction.options.getString('message');
-		await interaction.reply(message);
+
+		if (message.includes(['nigger', 'niggër', 'niggêr', 'nigg3r', 'nïgger', 'nïggër', 'nïggêr', 'nïgg3r', 'nîgger', 'nîggër', 'nîggêr', 'nîgg3r', 'n1gger', 'n1ggër', 'n1ggêr', 'n1gg3r'])) {
+			await interaction.reply({ content: 'This contains a bad word.', ephemeral: true });
+		} else if (interaction.user.permissions.has(Permissions.FLAGS.MANAGE_GUILD)) {
+			await interaction.reply(message);
+		} else {
+			await interaction.reply({ content: 'You are missing the **Manage Server** permission.\n (Needed to prevent spam and stop bad word usage, etc.)', ephemeral: true });
+		}
 	},
 };
