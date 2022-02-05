10ba0134a1efc9bec1c80f593a2492d314a93d7f
Radical <benjaminjohnsen.bgj@gmail.com>
2022-02-05 19:17:06 +0100

    Fix say part 3;

--- commands/say.js
+++ commands/say.js
@@ -1,6 +1,15 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const { Permissions } = require('discord.js');
 
+function contentcheck(message, filter) {
+	for (let i = 0; i < filter.length; i++) {
+		if (message.includes(filter[i])) {
+			return true;
+		}
+	}
+	return false;
+}
+
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('say')
@@ -9,7 +18,7 @@ module.exports = {
 	async execute(interaction) {
         const message = interaction.options.getString('message');
 
-		if (message.includes('nigger', 'niggër', 'niggêr', 'nigg3r', 'nïgger', 'nïggër', 'nïggêr', 'nïgg3r', 'nîgger', 'nîggër', 'nîggêr', 'nîgg3r', 'n1gger', 'n1ggër', 'n1ggêr', 'n1gg3r')) {
+		if (contentcheck({ message: message, filter: ['nigger', 'niggër', 'niggêr', 'nigg3r', 'nïgger', 'nïggër', 'nïggêr', 'nïgg3r', 'nîgger', 'nîggër', 'nîggêr', 'nîgg3r', 'n1gger', 'n1ggër', 'n1ggêr', 'n1gg3r'] })) {
 			await interaction.reply({ content: 'This contains a bad word.', ephemeral: true });
 		} else if (interaction.guild == null) {
 			await interaction.reply(message);
