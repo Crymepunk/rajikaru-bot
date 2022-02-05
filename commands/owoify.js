6b2fd943acb9fc25dc8b35e5fa34f72c52a68dd2
Radical <benjaminjohnsen.bgj@gmail.com>
2022-02-05 19:45:15 +0100

    Added bad word check

--- commands/owoify.js
+++ commands/owoify.js
@@ -2,6 +2,16 @@ const { SlashCommandBuilder } = require('@discordjs/builders');
 const client = require('nekos.life');
 const neko = new client();
 
+function contentcheck(message, filter) {
+	const len = filter.length;
+	for (let i = 0; i < len; i++) {
+		if (message.includes(filter[i])) {
+			return true;
+		}
+	}
+	return false;
+}
+
 function objToString(object) {
     let str = '';
     for (const k in object) {
@@ -20,7 +30,13 @@ module.exports = {
         .addStringOption(option => option.setName('message').setDescription('Message to say').setRequired(true)),
 	async execute(interaction) {
         let message = interaction.options.getString('message');
-        message = objToString(await neko.sfw.OwOify({ text: message }));
-		await interaction.reply(message);
+        const filter = ['nigger', 'niggër', 'niggêr', 'nigg3r', 'nïgger', 'nïggër', 'nïggêr', 'nïgg3r', 'nîgger', 'nîggër', 'nîggêr', 'nîgg3r', 'n1gger', 'n1ggër', 'n1ggêr', 'n1gg3r'];
+
+        if (contentcheck(message, filter)) {
+            await interaction.reply({ content: 'This conytainys a bad wowd.', ephemeral: true });
+        } else {
+            message = objToString(await neko.sfw.OwOify({ text: message }));
+            await interaction.reply(message);
+        }
 	},
 };
