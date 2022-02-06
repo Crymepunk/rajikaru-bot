8acbb033916d752ebfe16ca1db38a6fa201d2c61
Radical <benjaminjohnsen.bgj@gmail.com>
2022-02-06 17:36:47 +0100

    Make infractions an actual command

--- commands/infractions.js
+++ commands/infractions.js
@@ -1,5 +1,6 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
-const { userTables } = require('../functions');
+const { MessageEmbed } = require('discord.js');
+const { userTables, randomColor } = require('../functions');
 
 module.exports = {
 	data: new SlashCommandBuilder()
@@ -10,14 +11,109 @@ module.exports = {
         const user = interaction.options.getUser('user');
         const tableName = `${interaction.guild.id}-${user.id}`;
         const usertable = await userTables.findOne({ where: { name: tableName } });
-        try {
+        if (interaction.guild == null) {
+            return interaction.reply('This command only works in Guilds!');
+        } else if (usertable.get('infractions')) {
             console.log(usertable.get('infractions'));
-            const infractions = usertable.get('infractions');
-            interaction.reply(infractions);
-        } catch (error) {
-            console.log('An error has occurred');
-            console.error(error);
+            let infractions = usertable.get('infractions');
+            infractions = infractions.split('§');
+            let inf = '';
+            if (infractions.length >= 9) {
+                inf = (
+                    infractions[-10] + '\n'
+                    + infractions[-9] + '\n'
+                    + infractions[-8] + '\n'
+                    + infractions[-7] + '\n'
+                    + infractions[-6] + '\n'
+                    + infractions[-5] + '\n'
+                    + infractions[-4] + '\n'
+                    + infractions[-3] + '\n'
+                    + infractions[-2] + '\n'
+                    + infractions[-1] + '\n'
+                );
+            } else if (infractions.length == 8) {
+                inf = (
+                    infractions[-9] + '\n'
+                    + infractions[-8] + '\n'
+                    + infractions[-7] + '\n'
+                    + infractions[-6] + '\n'
+                    + infractions[-5] + '\n'
+                    + infractions[-4] + '\n'
+                    + infractions[-3] + '\n'
+                    + infractions[-2] + '\n'
+                    + infractions[-1] + '\n'
+                );
+            } else if (infractions.length == 7) {
+                inf = (
+                    infractions[-8] + '\n'
+                    + infractions[-7] + '\n'
+                    + infractions[-6] + '\n'
+                    + infractions[-5] + '\n'
+                    + infractions[-4] + '\n'
+                    + infractions[-3] + '\n'
+                    + infractions[-2] + '\n'
+                    + infractions[-1] + '\n'
+                );
+            } else if (infractions.length == 6) {
+                inf = (
+                    infractions[-7] + '\n'
+                    + infractions[-6] + '\n'
+                    + infractions[-5] + '\n'
+                    + infractions[-4] + '\n'
+                    + infractions[-3] + '\n'
+                    + infractions[-2] + '\n'
+                    + infractions[-1] + '\n'
+                );
+            } else if (infractions.length == 5) {
+                inf = (
+                    infractions[-6] + '\n'
+                    + infractions[-5] + '\n'
+                    + infractions[-4] + '\n'
+                    + infractions[-3] + '\n'
+                    + infractions[-2] + '\n'
+                    + infractions[-1] + '\n'
+                );
+            } else if (infractions.length == 4) {
+                inf = (
+                    infractions[-5] + '\n'
+                    + infractions[-4] + '\n'
+                    + infractions[-3] + '\n'
+                    + infractions[-2] + '\n'
+                    + infractions[-1] + '\n'
+                );
+            } else if (infractions.length == 3) {
+                inf = (
+                    infractions[-4] + '\n'
+                    + infractions[-3] + '\n'
+                    + infractions[-2] + '\n'
+                    + infractions[-1] + '\n'
+                );
+            } else if (infractions.length == 2) {
+                inf = (
+                    infractions[-3] + '\n'
+                    + infractions[-2] + '\n'
+                    + infractions[-1] + '\n'
+                );
+            } else if (infractions.length == 1) {
+                inf = (
+                    infractions[-2] + '\n'
+                    + infractions[-1] + '\n'
+                );
+            } else {
+                inf = (infractions[0] + '\n');
+            }
+
+            const infemb = new MessageEmbed()
+            .color(randomColor())
+            .setTitle(`${user.username}'s infractions`)
+            .addFields(
+                { name: 'Infractions', value: inf },
+            )
+            .setThumbnail('https://i.imgur.com/AfFp7pu.png');
+            interaction.reply({ embeds: [infemb] });
+        } else {
             interaction.reply('This user doesnt exist/doesnt have any infractions.');
         }
+
 	},
 };
