0874e994cda22d5c40c581d397bb9e8568bf20e5
Radiicall <66682497+Radiicall@users.noreply.github.com>
2022-03-02 05:02:56 +0100

    Change shards embed Forgot to stage changes before commiting

--- commands/developer/shards.js
+++ commands/developer/shards.js
@@ -8,12 +8,16 @@ module.exports = {
 	async execute(interaction) {
 		// Assign variables
 		const shards = await interaction.client.shard.fetchClientValues('guilds.cache.size');
+		// Construct embeds
 		let shemb = new MessageEmbed()
 			.setAuthor({ name: `${interaction.client.user.username}`, iconURL: `${interaction.client.user.avatarURL()}` })
 			.setColor('#01fff7');
+		// For loop for shards
         for (let i = 0; i < shards.length; i++) {
+			// Adds an inline field to the embed with the Shard int and how many guilds it's in.
             shemb = shemb.addField(`Shard ${i} has`, `${shards[i]} guilds.\n`, true);
         }
+		// Return embed
         return interaction.reply({ embeds: [shemb], ephemeral: true });
 	},
 };
